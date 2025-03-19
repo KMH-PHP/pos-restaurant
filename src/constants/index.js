09c01fa8d958207ed185e0cc62d4dc19f3179cd0
KMH-PHP <kyawminhtet754@gmail.com>
2025-03-19 23:43:19 +0630

    day1cooked

---
+++ src/constants/index.js
@@ -0,0 +1,62 @@
+export const popularDishes = [
+    {
+        id: 1,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 2,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 3,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 4,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 5,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 6,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 7,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 8,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 9,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+    {
+        id: 10,
+        image: 1,
+        name: 'Butter Chicken',
+        numberOfOrders: 250,
+    },
+]
