72d41126133e5c0fe643afd580dfbfaac390e5a9
KMH-PHP <kyawminhtet754@gmail.com>
2025-03-28 10:30:57 +0630

    day7

--- src/components/menu/Bill.jsx
+++ src/components/menu/Bill.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { getTotalPrice } from '../../redux/slices/cartSlice'
 
 const Bill = () => {
+
+  const cartData = useSelector((state) => state.cart);
+  const total = useSelector(getTotalPrice);
+  const taxRate = 5.25;
+  const tax = (total * taxRate) / 100;
+  const totalPriceWithTax = total + tax;
+
   return (
   <>
   <div className='flex items-center justify-between px-5 mt-2'>
-    <p className='text-xs text-[#ababab] font-medium mt-2'>Items(4)</p>
-    <h1 className='text-[#f5f5f5] text-md font-bold'>240</h1>
+    <p className='text-xs text-[#ababab] font-medium mt-2'>Items({cartData.length})</p>
+    <h1 className='text-[#f5f5f5] text-md font-bold'>${total}</h1>
   </div>
   <div className='flex items-center justify-between px-5 mt-2'>
     <p className='text-xs text-[#ababab] font-medium mt-2'>Taxs(5.25%)</p>
-    <h1 className='text-[#f5f5f5] text-md font-bold'>240</h1>
+    <h1 className='text-[#f5f5f5] text-md font-bold'>${tax}</h1>
+  </div>
+  <div className='flex items-center justify-between px-5 mt-2'>
+    <p className='text-xs text-[#ababab] font-medium mt-2'>Total with Tax</p>
+    <h1 className='text-[#f5f5f5] text-md font-bold'>${totalPriceWithTax}</h1>
   </div>
   <div className='flex items-center px-5 gap-3 mt-4'>
     <button className='text-[#ababab] bg-[#1f1f1f] px-4 py-3 w-full rounded-lg font-semibold'>Cash</button>
--- src/components/menu/CardInfo.jsx
+++ src/components/menu/CardInfo.jsx
@@ -1,62 +1,55 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { RiDeleteBin2Fill } from 'react-icons/ri'
 import { FaNotesMedical } from 'react-icons/fa'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeItems } from '../../redux/slices/cartSlice'
+
 const CardInfo = () => {
+
+  const cartData = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const scrollRef = useRef();
+
+  useEffect(() => {
+    if(scrollRef.current){
+      scrollRef.current.scrollTo({
+        top: scrollRef.current.scrollHeight,
+        behavior: 'smooth'
+      })
+    }
+  },[cartData])
+
+  const handleRemove = (itemId) => {
+    dispatch(removeItems(itemId));
+  }
   return (
     <div className='px-4 py-2'>
     <h1 className='text-lg text-[#4e4e4e] font-semibold tracking-wide'>Order details</h1>
-    <div className='mt-4 overflow-y-scroll scrollbar-hide h-[310px]'>
-      <div className='bg-[#1f1f1f] rounded-lg px-4 py-2 mb-2'>
-      <div className='flex items-center justify-between'>
-      <h1 className='text-md text-[#ababab] tracking-wide font-semibold'>
-        Chicken Tikka
-      </h1>
-      <p className='text-[#ababab] font-semibold '>x2</p>
-      </div>
-      <div className='flex items-center justify-between mt-3'>
-        <div className='flex items-center gap-3'>
-        <RiDeleteBin2Fill size={20} className='text-[#ababab] cursor-pointer'/>
-        <FaNotesMedical size={20} className='text-[#ababab] cursor-pointer'/>
-
-        </div>
-        <p className='text-md font-bold text-[#f5f5f5]'>$123</p>
-      </div>
-      </div>
+    <div className='mt-4 overflow-y-scroll scrollbar-hide  h-[310px]' ref={scrollRef}>
+      {cartData.length === 0 ? (
+        <p className='text-[#ababab] text-sm flex justify-center items-center h-[310px]'>Your cart is empty. Start adding items!</p>
+      ) : cartData.map((item, index) => {
+        return (
+          <div key={index} className='bg-[#1f1f1f] rounded-lg px-4 py-2 mb-2'>
+          <div className='flex items-center justify-between'>
+          <h1 className='text-md text-[#ababab] tracking-wide font-semibold'>
+            {item.name}
+          </h1>
+          <p className='text-[#ababab] font-semibold '>{item.quantity}</p>
+          </div>
+          <div className='flex items-center justify-between mt-3'>
+            <div className='flex items-center gap-3'>
+            <RiDeleteBin2Fill onClick={() => handleRemove(item.id)} size={20} className='text-[#ababab] cursor-pointer'/>
+            <FaNotesMedical size={20} className='text-[#ababab] cursor-pointer'/>
+    
+            </div>
+            <p className='text-md font-bold text-[#f5f5f5]'>${item.price}</p>
+          </div>
+          </div>
+        )})}
+     
       
-      <div className='bg-[#1f1f1f] rounded-lg px-4 py-2 mb-2'>
-      <div className='flex items-center justify-between'>
-      <h1 className='text-md text-[#ababab] tracking-wide font-semibold'>
-        Chicken Tikka
-      </h1>
-      <p className='text-[#ababab] font-semibold '>x2</p>
-      </div>
-      <div className='flex items-center justify-between mt-3'>
-        <div className='flex items-center gap-3'>
-        <RiDeleteBin2Fill size={20} className='text-[#ababab] cursor-pointer'/>
-        <FaNotesMedical size={20} className='text-[#ababab] cursor-pointer'/>
-
-        </div>
-        <p className='text-md font-bold text-[#f5f5f5]'>$123</p>
-      </div>
-      </div>
-      
-      <div className='bg-[#1f1f1f] rounded-lg px-4 py-2 mb-2'>
-      <div className='flex items-center justify-between'>
-      <h1 className='text-md text-[#ababab] tracking-wide font-semibold'>
-        Chicken Tikka
-      </h1>
-      <p className='text-[#ababab] font-semibold '>x2</p>
-      </div>
-      <div className='flex items-center justify-between mt-3'>
-        <div className='flex items-center gap-3'>
-        <RiDeleteBin2Fill size={20} className='text-[#ababab] cursor-pointer'/>
-        <FaNotesMedical size={20} className='text-[#ababab] cursor-pointer'/>
-
-        </div>
-        <p className='text-md font-bold text-[#f5f5f5]'>$123</p>
-      </div>
-      </div>
-      
+     
       
 
     </div>
